75de409ff649f2335d6f420d83a2cfcceb447ffe
Viktoriia Romanova <not.c0mp.uter.cat@gmail.com>
2025-03-20 14:45:36 +0200

    show all products

--- catalog/products/views.py
+++ catalog/products/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from products.models import Product, Category
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()
+    return render(request, 'index.html', {"products": products})
 
 def home(request):
     return render(request, 'base.html')
