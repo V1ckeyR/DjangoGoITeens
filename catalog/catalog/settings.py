d7fe3d7e92bb6cccfaf9515e81a7de897ec86f67
Viktoriia Romanova <not.c0mp.uter.cat@gmail.com>
2025-04-15 16:18:50 +0300

    10. Confirm email after registration

--- catalog/catalog/settings.py
+++ catalog/catalog/settings.py
@@ -127,3 +127,10 @@ MEDIA_ROOT = BASE_DIR / 'media'  # папка в проекті, де збері
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'your@gmail.com'      # ваша пошта
+# EMAIL_HOST_PASSWORD = 'yourpassword'    # пароль або спеціальний App Password при двофакторці
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = 'MyShop <your@gmail.com>'
